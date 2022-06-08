c9b5c86d771697b686f01c5abd297466756d275e
noahschlick <49171976+noahschlick@users.noreply.github.com>
2022-06-08 18:16:35 -0500

    new commit

--- components/Post.tsx
+++ components/Post.tsx
@@ -75,11 +75,11 @@ function Post({post}: Props) {
     })
   }
 
-  /*if(!post) return (
+  if(!post) return (
     <div className="flex w-full items-ceter justify-center p-10 text-xl">
       <Jelly size={50} color="#FF4501"/>
     </div>
-  )*/
+  )
   return (
     <Link href={`/post/${post.id}`}>
       <div className="flex cursor-pointer rounded-md border border-gray-300 bg-white
